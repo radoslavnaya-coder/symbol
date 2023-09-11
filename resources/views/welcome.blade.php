1357d03752cb49c70ae51fdd6f6be7968ea3c7ed
atimrish <atimrish11@gmail.com>
2023-09-11 16:32:19 +0400

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>The Symbolic Journal</title>
+  <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+  <header>
+
+  </header>
+  <section>
+  <img class="top-banner" src="images/top-banner.png">
+  <section class="banners-place">
+    <div class="firstsecond-main__category">
+      <img class="first-banner" src="images/first-banner.png">
+      <p class="banner-script">Politics</p>
+      <hr />
+      <img class="second-banner" src="images/second-banner.png">
+      <p class="banner-script">Food</p>
+    </div>
+    <hr />
+    <div class="third-main__category">
+      <img class="third-banner" src="images/third-banner.png">
+      <p class="third-banner-script">Travelling</p>
+    </div>
+  </section>
+  </section>
+  <footer>
+
+  </footer>
+</body>
+</html>
